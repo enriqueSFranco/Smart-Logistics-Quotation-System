045e5c19ae6a7aec3d1a490ac256e847b7f2d41d
enriqueSFranco <enrique.sfranco04@gmail.com>
2026-01-15 13:37:13 -0600

    delete files

--- backend/src/repositories/job-postgresql.repository.ts
+++
@@ -1,8 +0,0 @@
-// import occScraper from "../infrastructure/scrapers/OccScraper"
-// import glassdoorScraper from "../infrastructure/scrapers/GlassdoorScraper";
-// import { OCC_BASE_URL, GLASSDOOR_BASE_URL } from "../config/environment";
-
-export class JobPostgreSQLRepository {}
-
-const jobPostgreSQLRepository = new JobPostgreSQLRepository();
-export default jobPostgreSQLRepository;
